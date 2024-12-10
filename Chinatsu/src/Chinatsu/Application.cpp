f1db6e6a63000f5ce59ae4361ce7f17f5291b1fe
Surya Pratap Singh Chauhan <you@example.comsurya.pratap0038@gmail.com>
2024-12-10 21:28:38 +0530

    updated and working added custom fmt template

--- Chinatsu/src/Chinatsu/Application.cpp
+++ Chinatsu/src/Chinatsu/Application.cpp
@@ -1,4 +1,8 @@
 #include "Application.h"
+
+#include "Chinatsu/Events/ApplicationEvent.h"
+#include "Chinatsu/Log.h"
+
 namespace Chinatsu {
 	Application::Application()
 	{
@@ -7,7 +11,25 @@ namespace Chinatsu {
 	{
 	}
 	void Application::Run()
-	{
+	{	
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			CN_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			CN_TRACE(e);
+		}
 		while (true);
 	}
 }
+// Specialize fmt::formatter for WindowResizeEvent outside the Chinatsu namespace
+template <>
+struct fmt::formatter<Chinatsu::WindowResizeEvent> : fmt::formatter<std::string> {
+	// This function tells fmt how to format the custom WindowResizeEvent
+	template <typename FormatContext>
+	auto format(const Chinatsu::WindowResizeEvent& event, FormatContext& ctx) const {
+		return fmt::format_to(ctx.out(), "{} x {}", event.GetWidth(), event.GetHeight());
+	}
+};
