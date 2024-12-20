5138eb221d0212d110145a9b41a0289ad8e1397f
brodante <surya.pratap0038@gmail.com>
2024-12-21 05:02:34 +0530

    added glfw support, need to fix window close event

--- Chinatsu/src/Chinatsu/Application.cpp
+++ Chinatsu/src/Chinatsu/Application.cpp
@@ -6,33 +6,79 @@
 #include <GLFW/glfw3.h>
 
 namespace Chinatsu {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent)); // Bind the OnEvent function to the Application class
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		CN_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{	
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.5f, 0.0f, 0.5f, 1.0f); // Dark purple
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
 }
 // Specialize fmt::formatter for WindowResizeEvent outside the Chinatsu namespace
-template <>
-struct fmt::formatter<Chinatsu::WindowResizeEvent> : fmt::formatter<std::string>
-{
-	// This function tells fmt how to format the custom WindowResizeEvent
-	template <typename FormatContext>
-	auto format(const Chinatsu::WindowResizeEvent& event, FormatContext& ctx) const
-	{
-		return fmt::format_to(ctx.out(), "{} x {}", event.GetWidth(), event.GetHeight());
-	}
-};
+//template <>
+//struct fmt::formatter<Chinatsu::WindowResizeEvent> : fmt::formatter<std::string>
+//{
+//	// This function tells fmt how to format the custom WindowResizeEvent
+//	template <typename FormatContext>
+//	auto format(const Chinatsu::WindowResizeEvent& event, FormatContext& ctx) const
+//	{
+//		return fmt::format_to(ctx.out(), "{} x {}", event.GetWidth(), event.GetHeight());
+//	}
+//};
+
+
+// Base Event class
+//class Event {
+//public:
+//	virtual std::string ToString() const = 0;
+//};
+//
+//// Example of a derived event class
+//class WindowResizeEvent : public Event {
+//public:
+//	WindowResizeEvent(int width, int height)
+//		: m_Width(width), m_Height(height) {
+//	}
+//
+//	int GetWidth() const { return m_Width; }
+//	int GetHeight() const { return m_Height; }
+//
+//	std::string ToString() const override {
+//		return fmt::format("{} x {}", m_Width, m_Height);
+//	}
+//
+//private:
+//	int m_Width, m_Height;
+//};
+//
+//// Specialize fmt::formatter for the base Event class
+//template <>
+//struct fmt::formatter<Event> : fmt::formatter<std::string>
+//{
+//	// This function tells fmt how to format the custom Event
+//	template <typename FormatContext>
+//	auto format(const Event& event, FormatContext& ctx) const
+//	{
+//		return fmt::format_to(ctx.out(), "{}", event.ToString());
+//	}
+//};
--- Chinatsu/src/Chinatsu/Events/Event.h
+++ Chinatsu/src/Chinatsu/Events/Event.h
@@ -5,77 +5,103 @@
 
 namespace Chinatsu {
 
-	// Events in Chinatsu are currently blocking, meaning when an event occurs it
-	// immediately gets dispatched and must be dealt with right then an there.
-	// For the future, a better strategy might be to buffer events in an event
-	// bus and process them during the "event" part of the update stage.
+    // Events in Chinatsu are currently blocking, meaning when an event occurs it
+    // immediately gets dispatched and must be dealt with right then and there.
+    // For the future, a better strategy might be to buffer events in an event
+    // bus and process them during the "event" part of the update stage.
 
-	enum class EventType
-	{
-		None = 0,
-		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
-		AppTick, AppUpdate, AppRender,
-		KeyPressed, KeyReleased,
-		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
-	};
+    enum class EventType
+    {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+    };
 
-	enum EventCategory
-	{
-		None						= 0,
-		EventCategoryApplication	= BIT(0),
-		EventCategoryInput			= BIT(1),
-		EventCategoryKeyboard		= BIT(2),
-		EventCategoryMouse			= BIT(3),
-		EventCategoryMouseButton	= BIT(4)
-	};
+    enum EventCategory
+    {
+        None                        = 0,
+        EventCategoryApplication    = BIT(0),
+        EventCategoryInput          = BIT(1),
+        EventCategoryKeyboard       = BIT(2),
+        EventCategoryMouse          = BIT(3),
+        EventCategoryMouseButton    = BIT(4)
+    };
 
-	#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+    // Macro to generate EventType and GetName functionality
+    #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
 								virtual EventType GetEventType() const override { return GetStaticType(); }\
 								virtual const char* GetName() const override { return #type; }
 
-	#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+    #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
 
-	class CHINATSU_API Event
-	{
-		friend class EventDispatcher;
-	public:
-		virtual EventType GetEventType() const	= 0;
-		virtual const char* GetName() const		= 0;
-		virtual int GetCategoryFlags() const	= 0;
-		virtual std::string ToString() const	{ return GetName(); }
 
-		inline bool IsInCategory(EventCategory category)
-		{
-			return GetCategoryFlags() & category;
-		}
-	protected:
-		bool m_Handled = false;
-	};
+    class CHINATSU_API Event
+    {
+        friend class EventDispatcher;
+    public:
+        virtual EventType GetEventType() const  = 0;
+        virtual const char* GetName() const     = 0;
+        virtual int GetCategoryFlags() const    = 0;
+        virtual std::string ToString() const { return GetName(); }
 
-	class EventDispatcher
-	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
-	public:
-		EventDispatcher(Event& event) : m_Event(event){}
+        inline bool IsInCategory(EventCategory category)
+        {
+            return GetCategoryFlags() & category;
+        }
+    protected:
+        bool m_Handled = false;  // Flag to track if the event has been handled
+    };
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
-		{
-			if (m_Event.GetEventType() == T::GetStaticType())
-			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
-				return true;
-			}
-			return false;
-		}
+    // The EventDispatcher class processes and dispatches events
+    class EventDispatcher
+    {
+        template<typename T>
+        using EventFn = std::function<bool(T&)>;
+    public:
+        EventDispatcher(Event& event)
+            : m_Event(event)
+        {
+        }
 
-	private:
-		Event& m_Event;
-	};
+        template<typename T>
+        bool Dispatch(EventFn<T> func)
+        {
+            if (m_Event.GetEventType() == T::GetStaticType())
+            {
+                m_Event.m_Handled = func(*(T*)&m_Event);  // Cast the base event to derived type
+                return true;
+            }
+            return false;
+        }
 
-	inline std::ostream& operator<<(std::ostream& os, const Event& e)
-	{
-		return os << e.ToString();
-	}
+    private:
+        Event& m_Event;  // Reference to the event being dispatched
+    };
+
+    // Overload the << operator for printing events
+    inline std::ostream& operator<<(std::ostream& os, const Event& e)
+    {
+        return os << e.ToString();
+    }
+}
+
+// Specialize fmt::formatter for Event types outside the Chinatsu namespace
+template<typename T>
+struct fmt::formatter<
+    T, std::enable_if_t<std::is_base_of<Chinatsu::Event, T>::value, char>>
+    : fmt::formatter<std::string>
+{
+    auto format(const T& event, fmt::format_context& ctx) const
+    {
+        return fmt::format_to(ctx.out(), "{}", event.ToString());
+    }
+};
+
+// Utility function for formatting strings with arguments
+template <typename... T>
+std::string StringFromArgs(fmt::format_string<T...> fmt, T&&... args)
+{
+    return fmt::format(fmt, std::forward<T>(args)...);
 }
